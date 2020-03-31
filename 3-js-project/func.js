4de31765b30e7fdcf23e2bf50f39bb0900ff5e0c
s1dequest <maxzintel@gmail.com>
2020-03-30 21:46:49 -0400

    Cleaning things up a bit more. Need to resolve error of = after a 0.

--- 3-js-project/func.js
+++ 3-js-project/func.js
@@ -2,14 +2,33 @@ let result = document.querySelector('.result');
 let button = document.querySelectorAll('.button');
 let runningEquation = "0";
 
+// Following functions are just to shorten handleInput.
+// There is probably a better way to dynamically update...
+// ... the result and runningEq.
+function resNew(input) {
+  result.innerText = input;
+}
+
+function eqNew(input) {
+  runningEquation = input;
+}
+
+function eqUsed(input) {
+  runningEquation += input;
+}
+
+function resAfterNum(input) {
+  result.innerText += input;
+}
+
 function handleInput(input) {
   if(result.innerText === "0" && runningEquation === "0") {
-    result.innerText = input.target.innerText;
-    runningEquation = input.target.innerText;
+    resNew(input.target.innerText);
+    eqNew(input.target.innerText);
     console.log(runningEquation);
   } else if(result.innerText === "0" && runningEquation != "0") {
-    result.innerText = input.target.innerText;
-    runningEquation += input.target.innerText;
+    resNew(input.target.innerText);
+    eqUsed(input.target.innerText);
     console.log(runningEquation);
   } else {
     handleSymbol(input.target.innerText);
@@ -27,27 +46,36 @@ function init() {
 function handleSymbol(val) {
   switch(val) {
     case "C":
-      result.innerText = "0";
-      runningEquation = "0"
+      resNew("0");
+      eqNew("0");
       break;
     case "DEL": // Not working yet.
       break;
+    case "÷":
+      resNew("0");
+      eqUsed("/");
+      console.log(runningEquation)
+      break;
+    case "×":
+      resNew("0");
+      eqUsed("*");
+      console.log(runningEquation)
+      break;
     case "+":
     case "-":
-    case "÷":
-    case "×":
-      result.innerText = "0";
-      runningEquation += val;
+      resNew("0");
+      eqUsed(val);
       console.log(runningEquation)
       break;
     case "=": // Don't use eval.
-      result.innerText = Function('return ' + runningEquation)();
-      runningEquation = result.innerText;
+      resNew(Function('return ' + runningEquation)());
+      eqNew(result.innerText);
       console.log(runningEquation);
       break;
     default: // input is an additional digit to last number.
-      result.innerText += val;
-      runningEquation += val;
+      resAfterNum(val);
+      eqUsed(val);
+      console.log(runningEquation);
       break;
   };
 };
